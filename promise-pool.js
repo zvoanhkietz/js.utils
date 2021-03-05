a5fb097716e37e34003afca2951456cc02d4293c
zvoanhkietz <zvoanhkietz@gmail.com>
2021-03-05 08:57:35 +0700

    Update promise-pool.js

--- promise-pool.js
+++ promise-pool.js
@@ -1,21 +1,25 @@
 sleep = async (ts) => new Promise((rs)=> setTimeout(rs, ts));
+
 async function PromisePool(handler, data, concurency){
-    const result = [];
+    const results = [];
     workers = new Array(concurency)
         .fill(data.entries())
-        .map(handler);
+        .map(async (iterator) => {
+            for(const [index, item] of iterator){
+                console.log(`Request[${index}] info of ${item}`);
+                r = await handler(item);
+                results.push(r);
+                console.log(`Recv[${index}] info of ${item}`);
+                sleep(1000)
+            }
+        });
     await Promise.all(workers);
+    return results;
 }
 
-data = ["x", "b", "y", "f", "d", "e", "k", "g", "j", "l", "s"];
-results = [];
-await PromisePool(async (iterator) => {
-    for(const [index, item] of iterator){
-        console.log("Send:" + index + ":" + item);
-        const randomSecond = Math.floor(Math.random() * (2000 - 1000) ) + 1000;
-        await sleep(randomSecond);
-        console.log("Recv:" + index + ":" + item);
-        results.push(item);
-    }
+data = ["mojombo", "defunkt", "pjhyett", "wycats", "ezmobius", "ivey", "evanphx", "vanpelt", "wayneeseguin", "brynary", "kevinclark"];
+
+results = await PromisePool(async (item) => {
+    return await $.get(`https://api.github.com/users/${item}`)
 }, data, 3);
 console.log(results);
