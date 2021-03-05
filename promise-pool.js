75a4d439770b2d62c4981fcfd7b598f63a726860
zvoanhkietz <zvoanhkietz@gmail.com>
2021-03-05 08:14:31 +0700

    Update promise-pool.js

--- promise-pool.js
+++ promise-pool.js
@@ -1,15 +1,21 @@
 sleep = async (ts) => new Promise((rs)=> setTimeout(rs, ts));
+async function PromisePool(handler, data, concurency){
+    const result = [];
+    workers = new Array(concurency)
+        .fill(data.entries())
+        .map(handler);
+    await Promise.all(workers);
+}
 
 data = ["x", "b", "y", "f", "d", "e", "k", "g", "j", "l", "s"];
-concurency = 2;
-workers = new Array(concurency)
-    .fill(data.entries())
-    .map(async(iterator) => {
-        for(const [index, item] of iterator){
-            console.log("Send:" + index + ":" + item);
-            const randomSecond = Math.floor(Math.random() * (2000 - 1000) ) + 1000;
-            await sleep(randomSecond);
-            console.log("Recv:" + index + ":" + item);
-        }
-    });
-await Promise.all(workers);
+results = [];
+await PromisePool(async (iterator) => {
+    for(const [index, item] of iterator){
+        console.log("Send:" + index + ":" + item);
+        const randomSecond = Math.floor(Math.random() * (2000 - 1000) ) + 1000;
+        await sleep(randomSecond);
+        console.log("Recv:" + index + ":" + item);
+        results.push(item);
+    }
+}, data, 3);
+console.log(results);
