24fcecc20a8f5bd9944566c64d412a5c4cf02255
zvoanhkietz <zvoanhkietz@gmail.com>
2021-03-06 12:41:53 +0700

    Update promise-pool.js

--- promise-pool.js
+++ promise-pool.js
@@ -5,9 +5,7 @@ async function PromisePool(handler, data, concurency){
         .fill(data.entries())
         .map(async (iterator) => {
             for(const [index, item] of iterator){
-                console.log(`==>Request[${index}] info of ${item}`);
-                results.push(await handler(item));
-                console.log(`<=====Recv[${index}] info of ${item}`);
+                results.push(await handler(index, item));
             }
         });
     await Promise.all(workers);
@@ -17,9 +15,11 @@ async function PromisePool(handler, data, concurency){
 // RUN
 data = ["mojombo", "defunkt", "pjhyett", "wycats", "ezmobius", "ivey", "evanphx", "vanpelt", "wayneeseguin", "brynary", "kevinclark"];
 results = await PromisePool( async (item) => {
+    console.log(`==>Request[${index}] info of ${item}`);
     const rand = Math.floor(Math.random() * (3000 - 1000) + 1000);
     await sleep(rand);
     console.log(`Waiting response...${rand}s`);
+    console.log(`<=====Recv[${index}] info of ${item}`);
     return rand;
 }, data, 3);
 console.log(results);
