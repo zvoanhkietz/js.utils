74959f12c5f227a4fd063133ead92d17364f8989
zvoanhkietz <zvoanhkietz@gmail.com>
2021-03-06 12:38:14 +0700

    Update promise-pool.ts

--- promise-pool.ts
+++ promise-pool.ts
@@ -8,9 +8,7 @@ async function promisePool<T>(handler: Function, data: T[], concurency = 10): Pr
         .fill(data.entries())
         .map(async (iterator) => {
             for (const [index, item] of iterator) {
-                console.log(`==>Request[${index}] info of '${item}'`);
-                results.push(await handler(item));
-                console.log(`<=====Recv[${index}] info of '${item}'`);
+                results.push(await handler(index, item));
             }
         });
     await Promise.all(workers);
@@ -21,10 +19,12 @@ async function promisePool<T>(handler: Function, data: T[], concurency = 10): Pr
 // RUN
 (async () => {
     const data = ["mojombo", "defunkt", "pjhyett", "wycats", "ezmobius", "ivey", "evanphx", "vanpelt", "wayneeseguin", "brynary", "kevinclark"];
-    const results = await promisePool(async (item: string) => {
+    const results = await promisePool(async (index: number, item: string) => {
+        console.log(`==>Request[${index}] info of '${item}'`);
         const rand = Math.floor(Math.random() * (3000 - 1000) + 1000);
         await sleep(rand);
         console.log(`Waiting response from item '${item}'...${rand}s`);
+        console.log(`<=====Recv[${index}] info of '${item}'`);
         return item;
     }, data, 3);
     console.log(results);
