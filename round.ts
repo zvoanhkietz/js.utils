7b26cc92289d9f2156eac474ebf2d46b1d872a4b
zvoanhkietz <zvoanhkietz@gmail.com>
2021-03-22 05:59:46 +0700

    Update round.ts

--- round.ts
+++ round.ts
@@ -50,11 +50,9 @@ function roundToInt(value: number, mode: string) {
 }
 
 function round(value: number, precision = 0, mode = 'ROUND_HALF_UP') {
-    let p;
-
     value = castFloat(value);
     precision = castInt(precision);
-    p = Math.pow(10, precision);
+    let p = Math.pow(10, precision);
 
     if (isNaN(value) || !isFinite(value)) {
         return value;
