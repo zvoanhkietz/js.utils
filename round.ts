752fe4ac7b8c117bc1e03031192700211caab953
zvoanhkietz <zvoanhkietz@gmail.com>
2021-03-22 06:03:53 +0700

    Update round.ts

--- round.ts
+++ round.ts
@@ -1,41 +1,3 @@
-// refer: https://locutus.io/php/math/round/
-function castInt(value: number | string | boolean): number {
-    const type = typeof value
-
-    switch (type) {
-        case 'number':
-            if (isNaN(value as number) || !isFinite(value as number)) {
-                // from PHP 7, NaN and Infinity are casted to 0
-                return 0
-            }
-
-            return value < 0 ? Math.ceil(value as number) : Math.floor(value as number)
-        case 'string':
-            return parseInt(value as string, 10) || 0
-        case 'boolean':
-        // fall through
-        default:
-            return +!!value
-    }
-}
-
-function castFloat(value: number | string | boolean): number {
-    const type = typeof value
-
-    switch (type) {
-        case 'number':
-            return value as number
-        case 'string':
-            return parseFloat(value as string) || 0
-        case 'boolean':
-        // fall through
-        default:
-            // PHP docs state, that for types other than string
-            // conversion is {input type}->int->float
-            return castInt(value)
-    }
-}
-
 function roundToInt(value: number, mode: string) {
     let tmp = Math.floor(Math.abs(value) + 0.5)
 
@@ -50,8 +12,6 @@ function roundToInt(value: number, mode: string) {
 }
 
 function round(value: number, precision = 0, mode = 'ROUND_HALF_UP') {
-    value = castFloat(value);
-    precision = castInt(precision);
     let p = Math.pow(10, precision);
 
     if (isNaN(value) || !isFinite(value)) {
